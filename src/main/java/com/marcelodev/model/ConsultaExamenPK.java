f8db60bda9c7b6517c90d6bdd4a422a57dc53f9b
Marcelo Coldebella <marcelo.colde@gmail.com>
2021-10-02 20:58:22 -0300

    comentarios

--- src/main/java/com/marcelodev/model/ConsultaExamenPK.java
+++ src/main/java/com/marcelodev/model/ConsultaExamenPK.java
@@ -7,12 +7,18 @@ import javax.persistence.ManyToOne;
 @Embeddable
 public class ConsultaExamenPK {
 	
+	// implementar Serializable
+	
 	@ManyToOne
-	@JoinColumn(name = "id_examen")
+	@JoinColumn(name = "id_examen") // idExamen
 	private Examen examen;
 	
 	@ManyToOne
-	@JoinColumn(name = "id_consulta")
+	@JoinColumn(name = "id_consulta") // idConsulta
 	private Consulta consulta;
+	
+	/*
+	 * TODO - sobreescribir metodos equal and hashcode
+	 */
 	
 }
